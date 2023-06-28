36252fbd51a9e34ed40356a0736c45963f66d5b7
abyss <abissaldev@gmail.com>
2023-06-28 11:57:30 -0300

    mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
-import express from 'express';
-import { config } from 'dotenv';
+import express, { json } from 'express';
 import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import Joi from 'joi';
+import dayjs from 'dayjs';
 
-config();
-const app = express();
+const server = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// Middleware
+server.use(cors());
+server.use(json());
+dotenv.config();
 
-const client = new MongoClient(process.env.DATABASE_URL);
+// MongoDB Connection
+const databaseUrl = process.env.DATABASE_URL;
+const mongoClient = new MongoClient(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoClient.connect()
+  .then(() => {
+    const db = mongoClient.db();
+
+    // Start the server after successful database connection
+    server.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit the process with failure
+  });
