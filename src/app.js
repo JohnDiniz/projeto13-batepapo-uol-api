b2b61567c046815d2fb0ed6b8bf5d787f71dee05
abyss <abissaldev@gmail.com>
2023-07-03 14:08:54 -0300

    add messages post and get

--- src/app.js
+++ src/app.js
@@ -17,17 +17,13 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 const PORT = process.env.PORT;
 
 mongoClient.connect()
-  .then(() => {
-    db = mongoClient.db();
-    server.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  })
+  .then(() => db = mongoClient.db())
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
     process.exit(1); 
   });
 
+// Create a Or Add to MongoDB Connection
 server.post("/participants", async (req, res) => {
     try {
       const { name } = req.body;
@@ -40,15 +36,11 @@ server.post("/participants", async (req, res) => {
   
       const { error } = nameSchema.validate({ name });
   
-      if (error) {
-        return res.sendStatus(422);
-      }
-  
+      if (error) return res.sendStatus(422);
+    
       const existingUser = await db.collection("participants").findOne({ name });
   
-      if (existingUser) {
-        return res.sendStatus(409);
-      }
+      if (existingUser) return res.sendStatus(409);
   
       const newUser = {
         name,
@@ -74,8 +66,9 @@ server.post("/participants", async (req, res) => {
       return res.sendStatus(500);
     }
   });
-  
-  server.get('/participants', async (req, res) => {
+
+// Get Participants from MongoDB
+server.get('/participants', async (req, res) => {
     try {
         let participants = [];
         participants = await db.collection('participants').find().toArray();
@@ -84,4 +77,74 @@ server.post("/participants", async (req, res) => {
         console.error(error);
         res.sendStatus(500);
     }
+});
+
+server.post('/messages', async (req, res) => {
+  try {
+      const { to, text, type } = req.body;
+      const { user } = req.headers;
+      console.log(user);
+      console.log(to);
+      console.log(text);
+      console.log(type);
+      const messageSchema = joi.object({
+          to: joi.string().required(),
+          text: joi.string().required(),
+          type: joi.string().required().valid("message").valid("private_message"),
+      })
+      const validate = messageSchema.validate(req.body);
+      console.log(validate.error)
+      if (validate.error) return res.sendStatus(422);
+      const validateUser = await db.collection("participants").findOne({ name: user });
+      console.log(validateUser);
+      if (!validateUser) {
+          return res.sendStatus(422);
+      }
+
+      const now = dayjs().format("HH:mm:ss");
+
+      const newMessage = {
+          from: user,
+          to: to,
+          text: text,
+          type: type,
+          time: now
+      };
+
+      await db.collection("messages").insertOne(newMessage);
+
+      return res.sendStatus(201);
+  } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+  }
+
+});
+
+server.get('/messages', async (req, res) => {
+  try {
+      const { user } = req.headers;
+      const limit = req.query.limit;
+      if ((Number(limit) <= 0 || isNaN(limit)) && limit !== undefined) {
+          return res.sendStatus(422);
+      }
+      let messages = [];
+      messages = await db.collection('messages').find({ $or: [{ $or: [{ to: user }, { from: user }] }, { $or: [{ to: "Todos" }, { from: "Todos" }] }] }).toArray();
+
+      if (Number(limit)) {
+          const tamanho = messages.length;
+          if (Number(limit) < tamanho) {
+              return res.send(messages.slice(tamanho - Number(limit)));
+          }
+      }
+      res.send(messages);
+  } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+  }
+});
+
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
